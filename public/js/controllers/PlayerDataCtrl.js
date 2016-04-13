5771a4b52b3a94b25df54e113feeee61b9ca7cd4
Chip Keyes <chip.keyes88@gmail.com>
2016-04-12 20:26:03 -0700

    Removed a console.log statement.

--- public/js/controllers/PlayerDataCtrl.js
+++ public/js/controllers/PlayerDataCtrl.js
@@ -9,11 +9,14 @@ baseballApp.controller('PlayerDataCtrl',
         scatter: {
           onlyCircles: true
         },
+        showLegend: false,
         showDistX: true,
         showDistY: true,
-        //tooltipContent: function(d) {
-        //    return d.series && '<h3>' + d.series[0].key + '</h3>';
-        //},
+        useInteractiveGuideline: false,
+        interactive: true,
+        tooltipContentFunction: function(key, x, y, e, graph) {
+          return "<h1>" + key + "</h3>" + "<p>" + x + ":" + y + "</p>";
+        },
         duration: 350,
         xAxis: {
           axisLabel: 'Hit Count',
@@ -27,55 +30,44 @@ baseballApp.controller('PlayerDataCtrl',
             return d3.format('.02f')(d);
           },
           axisLabelDistance: -5
-        },
-        zoom: {
-          //NOTE: All attributes below are optional
-          enabled: true,
-          scaleExtent: [1, 10],
-          useFixedDomain: false,
-          useNiceScale: false,
-          horizontalOff: false,
-          verticalOff: false,
-          unzoomEventType: 'dblclick.zoom'
         }
       }
     };
 
     $scope.data = generateData();
 
+
     /* Random Data Generator (took from nvd3.org) */
     function generateData() {
       var playerData = [];
 
       playersService.getPlayers(function(data) {
-        console.log(data);
-        data.forEach(function(currentPlayer, index, array) {
-          var player = {
-            name: currentPlayer.playername,
-            aveStats: {
-              ballsfaced: 0,
-              hitcount: 0,
-              innings: 0,
-              games: 0
-            }
-          };
-          currentPlayer.gamestats.forEach(function(currentGame, index, array) {
-            player.aveStats.ballsfaced += currentGame.ballsfaced;
-            player.aveStats.hitcount += currentGame.hitcount;
-            player.aveStats.innings += currentGame.innings;
-            player.aveStats.games++;
-          });
 
+        data.forEach(function(currentPlayer, index, array) {
+
+          var name = currentPlayer.playername;
+          var ballCount = 0,
+            hitCount = 0,
+            inningCount = 0,
+            games = 0;
+          currentPlayer.gamestats.forEach(function(currentGame, index, array) {
+
+            ballCount += currentGame.ballsfaced;
+            hitCount += currentGame.hitcount;
+            inningCount += currentGame.innings;
+            games++;
+          });
+          
           playerData.push({
-            key: player.playername,
+            key: name,
             values: [{
-              x: (player.aveStats.ballsfaced / player.aveStats.games),
-              y: (player.aveStats.hitcount / player.aveStats.games),
-              size: (player.aveStats.innings / player.aveStats.games)
+              x: (ballCount / games),
+              y: (hitCount / games),
             }]
           });
         });
       });
+
       return playerData;
     };
 
